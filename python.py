14fb703492e3da35548bddef43524052e7ba2026
Shannon Millar <shanenak@gmail.com>
2023-12-14 10:02:06 -0800

    mini max sum

--- python.py
+++ python.py
@@ -13,20 +13,33 @@ import sys
 #
 
 
-def plusMinus(arr):
+# def plusMinus(arr):
+#     # Write your code here
+#     length = len(arr)
+#     pos = len([i for i in arr if i > 0])
+#     zer = len([i for i in arr if i == 0])
+#     neg = len([i for i in arr if i < 0])
+#     print("{:.6f}".format(pos/length))
+#     print("{:.6f}".format(neg/length))
+#     print("{:.6f}".format(zer/length))
+
+
+# if __name__ == '__main__':
+#     n = int(input().strip())
+
+#     arr = list(map(int, input().rstrip().split()))
+
+#     plusMinus(arr)
+
+
+def miniMaxSum(arr):
     # Write your code here
-    length = len(arr)
-    pos = len([i for i in arr if i > 0])
-    zer = len([i for i in arr if i == 0])
-    neg = len([i for i in arr if i < 0])
-    print("{:.6f}".format(pos/length))
-    print("{:.6f}".format(neg/length))
-    print("{:.6f}".format(zer/length))
+    arr.sort()
+    print(sum(arr[:4]), sum(arr[1:]))
 
 
 if __name__ == '__main__':
-    n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
 
-    plusMinus(arr)
+    miniMaxSum(arr)
